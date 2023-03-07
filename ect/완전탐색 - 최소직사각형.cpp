cf72aca6a7b845da9d72c3ccc42beff2b4e65839
unknown <sikk806@gmail.com>
2023-03-07 15:35:08 +0900

    최소직사각형

---
+++ ect/완전탐색 - 최소직사각형.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    
+    int max_w = 0;
+    int max_h = 0;
+    
+    for(int i = 0; i < sizes.size(); i++) {
+        if(sizes[i][0] < sizes[i][1]) {
+            int temp = sizes[i][0];
+            sizes[i][0] = sizes[i][1];
+            sizes[i][1] = temp;
+        }
+        
+        if(max_w < sizes[i][0]) max_w = sizes[i][0];
+        if(max_h < sizes[i][1]) max_h = sizes[i][1];
+    }
+    
+    answer = max_w * max_h;
+    
+    return answer;
+}
